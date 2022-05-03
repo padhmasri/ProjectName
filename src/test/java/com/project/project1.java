e072d9c58b0cb29e2d109c0897f21ef38d1d2930
padhmasri <padhmasri20@gmail.com>
2022-05-03 21:23:18 +0530

    project1

---
+++ src/test/java/com/project/project1.java
@@ -0,0 +1,25 @@
+package com.project;
+
+public class project1 {
+	
+	private void method1() {
+		System.out.println("m1");
+
+	}
+	
+	private void method2() {
+		System.out.println("m2");
+
+	}
+	
+	private void method3() {
+		System.out.println("m3");
+
+	}
+	
+	private void method4() {
+		System.out.println("m4");
+
+	}
+
+}
